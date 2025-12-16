cedfb68ebd24cf7a8ac2b5be1428c79d0fa55cd4
Brenen Olson <brnolson33@gmail.com>
2025-12-15 20:06:45 -0600

    Revert "IOS Patch 4"
    This reverts commit 712bef43a95fbb9540f6125d09b8b972852a6efe.

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,4 @@ import Toast from 'primevue/toast'
   <RouterView />
 </template>
 
-<style scoped>
-  body, #app {
-  position: relative;
-  z-index: 1;
-  -webkit-transform: translateZ(0);
-  transform: translateZ(0);
-  background: white !important;
-}
-</style>
+<style scoped></style>
