712bef43a95fbb9540f6125d09b8b972852a6efe
Brenen Olson <brnolson33@gmail.com>
2025-12-15 19:59:43 -0600

    IOS Patch 4

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,12 @@ import Toast from 'primevue/toast'
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+  body, #app {
+  position: relative;
+  z-index: 1;
+  -webkit-transform: translateZ(0);
+  transform: translateZ(0);
+  background: white !important;
+}
+</style>
